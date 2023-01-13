cef17dfbd8d7abbcdecd2e5384652614cc94e80b
Fusion-Flux <c4gamer2.0@gmail.com>
2023-01-12 18:23:44 -0600

    Rename

---
+++ ProudHearthians/ProudHearthians.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using OWML.Common;
+using OWML.ModHelper;
+using UnityEngine;
+
+namespace ProudHearthians;
+
+public class ProudHearthians : ModBehaviour
+{
+	private void Awake()
+	{
+		// You won't be able to access OWML's mod helper in Awake.
+		// So you probably don't want to do anything here.
+		// Use Start() instead.
+	}
+
+	private void Start()
+	{
+		// Starting here, you'll have access to OWML's mod helper.
+		ModHelper.Console.WriteLine($"My mod {nameof(ProudHearthians)} is loaded!", MessageType.Success);
+
+
+		// Example of accessing game code.
+		LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+		{
+			if (loadScene != OWScene.SolarSystem) return;
+			
+			var materials = Resources.FindObjectsOfTypeAll<Material>().Where(x => x.name == "Traveller_HEA_PlayerSuit_mat");
+			foreach (var material in materials)
+			{
+				material.mainTexture = ModHelper.Assets.GetTexture("queer.png");
+				material.SetTexture("_BumpMap", ModHelper.Assets.GetTexture("queerNormal.png"));
+			}
+		};
+	}
+}
+
